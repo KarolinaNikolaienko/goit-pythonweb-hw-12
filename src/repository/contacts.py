18147f3f08b6330f3c21da55e0e16bc0ca746ee6
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2025-02-17 05:45:45 +0100

    added documentation

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -13,19 +13,56 @@ from src.schemas.contacts import ContactBase, ContactResponse
 
 class ContactRepository:
     def __init__(self, session: AsyncSession):
+        """
+        Initialize a ContactRepository.
+
+        Args:
+            session: An AsyncSession object connected to the database.
+        """
         self.db = session
 
     async def get_contacts(self, skip: int, limit: int, user: User) -> List[Contact]:
+        """
+        Get a list of Contacts owned by `user` with pagination.
+
+        Args:
+            skip: The number of Contacts to skip.
+            limit: The maximum number of Contacts to return.
+            user: The owner of the Contacts to retrieve.
+
+        Returns:
+            A list of Contacts.
+        """
         stmt = select(Contact).filter_by(user=user).offset(skip).limit(limit)
         contacts = await self.db.execute(stmt)
         return contacts.scalars().all()
 
     async def get_contact_by_id(self, contact_id: int, user: User) -> Contact | None:
+        """
+        Get a Contact by its id.
+
+        Args:
+            contact_id: The id of the Contact to retrieve.
+            user: The owner of the Contact to retrieve.
+
+        Returns:
+            The Contact with the specified id, or None if no such Contact exists.
+        """
         stmt = select(Contact).filter_by(id=contact_id, user=user)
         contact = await self.db.execute(stmt)
         return contact.scalar_one_or_none()
 
     async def search_contact(self, q: str, skip: int, limit: int, user: User):
+        """
+        Get a Contact by its field value.
+
+        Args:
+            q: Query string to search in fields.
+            user: The owner of the Contact to retrieve.
+
+        Returns:
+            The Contact with the field, which has query value.
+        """
         stmt = (
             select(Contact)
             .filter_by(user=user)
@@ -46,6 +83,18 @@ class ContactRepository:
     async def get_birthdays(
         self, days: int, skip: int, limit: int, user: User
     ) -> List[Contact]:
+        """
+        Get list of contacts, who have birthday on the next x days.
+
+        Args:
+            days: Number of days.
+            skip: The number of Contacts to skip.
+            limit: The maximum number of Contacts to return.
+            user: The User who owns the Contact.
+
+        Returns:
+            A list of Contacts.
+        """
         stmt = (
             select(Contact)
             .filter_by(user=user)
@@ -67,6 +116,16 @@ class ContactRepository:
         return contacts.scalars().all()
 
     async def create_contact(self, body: ContactBase, user: User) -> Contact:
+        """
+        Create a new Contact with the given attributes.
+
+        Args:
+            body: A ContactBase with the attributes to assign to the Contact.
+            user: The User who owns the Contact.
+
+        Returns:
+            A Contact with the assigned attributes.
+        """
         contact = Contact(**body.model_dump(exclude_unset=True), user=user)
         self.db.add(contact)
         await self.db.commit()
@@ -74,6 +133,16 @@ class ContactRepository:
         return await self.get_contact_by_id(contact.id, user)
 
     async def delete_contact(self, contact_id: int, user: User) -> Contact | None:
+        """
+        Delete a Contact by its id.
+
+        Args:
+            contact_id: The id of the Contact to delete.
+            user: The owner of the Contact to delete.
+
+        Returns:
+            The deleted Contact, or None if no Contact with the given id exists.
+        """
         contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             await self.db.delete(contact)
@@ -83,6 +152,17 @@ class ContactRepository:
     async def update_contact(
         self, contact_id: int, body: ContactBase, user: User
     ) -> Contact | None:
+        """
+        Update a Contact with the given attributes.
+
+        Args:
+            contact_id: The id of the Contact to update.
+            body: A ContactBase with the attributes to assign to the Contact.
+            user: The User who owns the Contact.
+
+        Returns:
+            The updated Contact, or None if no Contact with the given id exists.
+        """
         contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             for key, value in body.model_dump(exclude_unset=True).items():
--- src/repository/users.py
+++ src/repository/users.py
@@ -10,21 +10,58 @@ class UserRepository:
         self.db = session
 
     async def get_user_by_id(self, user_id: int) -> User | None:
+        """
+        Get a User by its id.
+
+        Args:
+            user_id: The id of the User to retrieve.
+
+        Returns:
+            The User with the specified id, or None if no such User exists.
+        """
         stmt = select(User).filter_by(id=user_id)
         user = await self.db.execute(stmt)
         return user.scalar_one_or_none()
 
     async def get_user_by_username(self, username: str) -> User | None:
+        """
+        Get a User by its username.
+
+        Args:
+            username: The username of the User to retrieve.
+
+        Returns:
+            The User with the specified username, or None if no such User exists.
+        """
         stmt = select(User).filter_by(username=username)
         user = await self.db.execute(stmt)
         return user.scalar_one_or_none()
 
     async def get_user_by_email(self, email: str) -> User | None:
+        """
+        Get a User by its email.
+
+        Args:
+            email: The username of the User to retrieve.
+
+        Returns:
+            The User with the specified email, or None if no such User exists.
+        """
         stmt = select(User).filter_by(email=email)
         user = await self.db.execute(stmt)
         return user.scalar_one_or_none()
 
     async def create_user(self, body: UserCreate, avatar: str = None) -> User:
+        """
+        Create a new User with the given attributes.
+
+        Args:
+            body: A UserCreate with the attributes to assign to the User.
+            avatar: Generated avatar image in str format.
+
+        Returns:
+            A User with the assigned attributes.
+        """
         user = User(
             **body.model_dump(exclude_unset=True, exclude={"password"}),
             hashed_password=body.password,
@@ -36,6 +73,15 @@ class UserRepository:
         return user
 
     async def confirmed_email(self, email: str) -> None:
+        """
+        Sets User's 'confirmed' attribute to True.
+
+        Args:
+            email: Email of the User
+
+        Returns:
+            None
+        """
         user = await self.get_user_by_email(email)
         user.confirmed = True
         await self.db.commit()
